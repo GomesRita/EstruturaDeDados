6f624f48d088e37377e91a1c494a10283b660865
Rita Gomes <94627735+GomesRita@users.noreply.github.com>
2024-05-27 16:01:30 +0000

    Função Consultar Contatos pronta

--- TAD/agenda_imlementacao.ts
+++ TAD/agenda_imlementacao.ts
@@ -5,19 +5,32 @@ class Agenda{
         var novoContato = new Pessoa(nome, endereco, telefone)
         this.listaAgenda.push(novoContato);
     }
-    ConsultarContatos(){
-
-        
+    ConsultarContatos(Nome: string){
+        console.log(`Resultado da Busca:\n`)
+        let busca = false
+        for(var i = 0; i < this.listaAgenda.length; i++ ){
+            if(this.listaAgenda[i].nome === Nome){
+                console.log(`Nome: ${this.listaAgenda[i].nome}  Telefone: ${this.listaAgenda[i].telefone}  Endereço: ${this.listaAgenda[i].endereco}`)
+                busca = true
+            }
+        } 
+        if(busca !== true){
+            console.log("Registro não encontrado")
+        }
+      
     }
     ExibirContatos(){
         this.listaAgenda.forEach(function (value: Pessoa){
             console.log(`Nome: ${value.nome}   Telefone: ${value.telefone}    Endereço: ${value.endereco}`)
         });
     }
-    ExcluirContato(){}
+    ExcluirContato(){
+
+    }
 }
 
 let agenda = new Agenda()
 agenda.InserirContato('Rita','AAA','9991111222');
 agenda.InserirContato('Maria','A2A','444555888');
 agenda.ExibirContatos();
+agenda.ConsultarContatos('Maria')
