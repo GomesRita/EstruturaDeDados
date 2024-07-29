f0fa1c325655cad36619894d2fe31ed4accf08c9
Rita Gomes <94627735+GomesRita@users.noreply.github.com>
2024-07-29 19:17:46 +0000

    questão 12

--- TAD/Avaliacao/Q12.js
+++ TAD/Avaliacao/Q12.js
@@ -6,31 +6,30 @@ elementos comuns aos dois arrays, sem duplicatas.
 class questao12{
 
     constructor(){
-        this.lista1 = new Array(1,2,3,4,5)
-        this.lista2 = new Array(1,7,3,5,6)
-        this.repeticoes = 0
+        this.lista1 = [1,2,3,4,5]
+        this.lista2 = [1,2,3,5,6]
+        this.repeticoes = [1]
     }
 
     verificar(){
+
         for(var i = 0; i < this.lista1.length; i++){
 
             for(var j = 0; j < this.lista2.length; j++){
-                
                 if(this.lista1[i] === this.lista2[j]){
-                    this.repeticoes = this.repeticoes +1
-                    return true
-                } else {
-                    return false
-                }
+                    if(this.repeticoes.includes(this.lista1[i]) === false){
+                        this.repeticoes.push(this.lista1[i])
+                    }
+                } 
 
             } 
         }
-        console.log(this.repeticoes)
+        console.log("ITENS EM COMUM NAS DUAS LISTAS \n" + this.repeticoes + "\nTotal de itens comuns: " + this.repeticoes.length)
     }
 
 }
 
-let questao = new questao12()
-questao.verificar()
+let questao = new questao12();
+questao.verificar();
 
 
